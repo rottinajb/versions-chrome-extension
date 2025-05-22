85be335b16bc20315b615fd17f90e07dee9f6fd9
Mike Rottina <michael.rottina@jetblue.com>
2025-05-21 21:39:48 -0400

    Get versions from API instead of HTML

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Dotcom environments chrome extension
 
 A useful little tool to quickly see what Dotcom version is in which environment.
+
+## Installation
+
+1. Visit: https://github.com/rottinajb/versions-chrome-extension
+2. Click the green Code button.
+3. CLick Download ZIP, and unzip to a location of your choice.
+4. In the chrome browser, type `chrome://extensions`
+5. In the upper right cornder ensure "Developer mode" is enabled.
+6. On the top left, click "Load Unpacked".
+7. Choose the folder you unzipped into.
+8. Click "Select"
+9. Click the extensions icon in chrome.
+10. Pin the Versions extension by clicking the pin symbol.
--- popup.js
+++ popup.js
@@ -1,23 +1,22 @@
 async function getVersions() {
-	const prd = "https://dotcom.jetblue.com";
-	const nprd = "https://dotcom-nprd.jetblue.com";
+	const prd = "dotcom";
+	const nprd = "dotcom-nprd";
 	const envs = [prd, nprd];
 
 	for (const env of envs) {
 		console.log(env);
 		try {
-			const response = await fetch(env);
+			const response = await fetch(`https://${env}.jetblue.com/api/version`);
 			if (!response.ok) {
 				throw new Error(`HTTP error! status: ${response.status}`);
 			}
-			const html = await response.text();
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(html, "text/html");
-			const ver = doc.querySelector('meta[name="version"]').content;
+			const ver = await response.text();
+			// return data;
 			console.log(ver);
 			printVersions(ver, env);
 		} catch (error) {
 			console.error("Failed to fetch:", error);
+			return null;
 		}
 	}
 }
