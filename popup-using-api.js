76ede67ff65b5bb43b7a1b112c953065f5795b86
Mike Rottina <michael.rottina@jetblue.com>
2025-05-30 10:47:15 -0400

    Using dom parsin instead of api for now

--- popup-using-api.js
+++ popup-using-api.js
@@ -1,23 +1,26 @@
+//keeping this as is, currently we are blocking public access to dotcom.jetblue.com, so
+//we need to take a different approach and look for the meta tag instead of using the api
+// when we can this is the better approach
+
 async function getVersions() {
-	const prd = "https://www.jetblue.com/flying-with-us";
-	const nprd = "https://dotcom-nprd.jetblue.com";
+	const prd = "dotcom";
+	const nprd = "dotcom-nprd";
 	const envs = [prd, nprd];
 
 	for (const env of envs) {
 		console.log(env);
 		try {
-			const response = await fetch(env);
+			const response = await fetch(`https://${env}.jetblue.com/api/version`);
 			if (!response.ok) {
 				throw new Error(`HTTP error! status: ${response.status}`);
 			}
-			const html = await response.text();
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(html, "text/html");
-			const ver = doc.querySelector('meta[name="version"]').content;
+			const ver = await response.text();
+			// return data;
 			console.log(ver);
 			printVersions(ver, env);
 		} catch (error) {
 			console.error("Failed to fetch:", error);
+			return null;
 		}
 	}
 }
